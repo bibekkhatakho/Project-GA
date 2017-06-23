31d51b262021ac3bc1abecb659b1b602d32b456d
Chris <iodfuse@gmail.com>
2017-06-23 15:22:44 -0500

    Added basic support for firebase realtime database.

--- app/src/main/java/com/project/group/projectga/MainActivity.java
+++ app/src/main/java/com/project/group/projectga/MainActivity.java
@@ -2,12 +2,57 @@ package com.project.group.projectga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.*;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.ProjectGA.MESSAGE";
+
+    String message; // input used for the test, this is saved to the database by the save button
+    String result;  // string retrieved from the database, this is displayed by the load button
+    DatabaseReference DbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // reference to "message" location in the database
+        DbReference = FirebaseDatabase.getInstance().getReference("message");
+
+        // Read from the database when "message" location is changed
+        DbReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                result = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+    }
+
+    /**
+     * Called when the user taps the Test button
+     */
+    public void saveButton(View view) {
+        // save the contents of the editText field to the "message" location of the database
+        EditText editText = (EditText) findViewById(R.id.editText);
+        message = editText.getText().toString();
+
+        DbReference.setValue(message);
+    }
+
+    public void loadButton(View view) {
+        // display the contents of the "message" location in the textView
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(result);
     }
 }
