af3159eaa6cd9deb3b03c8cac7526c8e0c1ef7f7
Chris <iodfuse@gmail.com>
2017-06-23 15:23:12 -0500

    Added basic support for firebase realtime database.

---
+++ app/src/main/java/com/project/group/projectga/Profile.java
@@ -0,0 +1,65 @@
+package com.project.group.projectga;
+
+/**
+ * Created by iod on 6/23/2017.
+ */
+
+public class Profile {
+    private String email;
+    private String password;
+    private String name;
+    private int birthYear;
+    private int birthMonth;
+    private int birthDay;
+
+    public Profile() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(int birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(int birthDay) {
+        this.birthDay = birthDay;
+    }
+}
