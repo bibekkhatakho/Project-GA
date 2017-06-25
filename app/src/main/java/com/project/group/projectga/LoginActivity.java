6c31508921d437e4e35e0c07e837117ac8788190
bibek <bibek.nsty@gmail.com>
2017-06-25 18:18:14 -0500

    some changes in the gui of login

--- app/src/main/java/com/project/group/projectga/LoginActivity.java
+++ app/src/main/java/com/project/group/projectga/LoginActivity.java
@@ -2,12 +2,24 @@ package com.project.group.projectga;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View v){
+        EditText email = (EditText)findViewById(R.id.emailET);
+        EditText password = (EditText)findViewById(R.id.emailET);
+        if(v.getId() == R.id.loginButton){
+
+
+        }
     }
 }
