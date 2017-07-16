976682e915c5b17e74ece18462d5f96ad1f376c8
RaMaJ123 <ramji.seetharaman@mavs.uta.edu>
2017-07-16 09:47:44 -0500

    Gallery Folder Structure

---
+++ app/src/main/java/com/project/group/projectga/adapters/MyGridAdapter.java
@@ -0,0 +1,78 @@
+package com.project.group.projectga.adapters;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.project.group.projectga.R;
+import com.project.group.projectga.models.GridViewItem;
+
+import java.util.List;
+
+/**
+ * Created by ramjiseetharaman on 7/15/17.
+ */
+
+    public class MyGridAdapter extends BaseAdapter {
+
+        LayoutInflater inflater;
+        List<GridViewItem> items;
+        private Context mContext;
+
+
+        public MyGridAdapter(Context context, List<GridViewItem> items) {
+            mContext = context;
+            this.items = items;
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.grid_item, null);
+            }
+
+            TextView text = (TextView) convertView.findViewById(R.id.textView);
+            text.setText(items.get(position).getPath());
+
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            Bitmap image = items.get(position).getImage();
+
+            if (image != null){
+                imageView.setImageBitmap(image);
+            }
+            else {
+                // If no image is provided, display a folder icon.
+                imageView.setImageResource(R.drawable.ic_folder_grey_24dp);
+            }
+
+            return convertView;
+        }
+
+    }
--- app/src/main/java/com/project/group/projectga/fragments/GalleryFragment.java
+++ app/src/main/java/com/project/group/projectga/fragments/GalleryFragment.java
@@ -5,10 +5,12 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -34,9 +36,15 @@ import android.widget.Toast;
 import com.project.group.projectga.R;
 import com.project.group.projectga.activities.PhotosActivity;
 import com.project.group.projectga.adapters.Adapter_PhotosFolder;
+import com.project.group.projectga.adapters.MyGridAdapter;
+import com.project.group.projectga.helpers.BitmapHelper;
+import com.project.group.projectga.models.GridViewItem;
 import com.project.group.projectga.models.Model_images;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
@@ -47,6 +55,9 @@ public class GalleryFragment extends Fragment {
     Adapter_PhotosFolder obj_adapter;
     GridView gv_folder;
     private static final int REQUEST_PERMISSIONS = 100;
+
+    List<GridViewItem> gridItems;
+    GridView gridView;
 
     public GalleryFragment(){
 
@@ -71,34 +82,38 @@ public class GalleryFragment extends Fragment {
         toolbar.setBackground(getResources().getDrawable(R.drawable.tile_green));
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu_green_24dp));
 
+        gridView = (GridView) view.findViewById(R.id.gridView);
 
-        gv_folder = (GridView)view.findViewById(R.id.gv_folder);
-        gv_folder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getContext(), PhotosActivity.class);
-                intent.putExtra("value",i);
-                startActivity(intent);
-            }
-        });
+        setGridAdapter(Environment.getExternalStorageDirectory().getAbsolutePath());
 
-        if ((ContextCompat.checkSelfPermission(getContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
-            if ((ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                    Manifest.permission.READ_EXTERNAL_STORAGE))) {
 
-            } else {
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_PERMISSIONS);
-            }
-        }else {
-            Log.e("Else","Else");
-            fn_imagespath();
-        }
-		
+//        gv_folder = (GridView)view.findViewById(R.id.gv_folder);
+//        gv_folder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(getContext(), PhotosActivity.class);
+//                intent.putExtra("value",i);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        if ((ContextCompat.checkSelfPermission(getContext(),
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getContext(),
+//                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+//            if ((ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+//                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+//                    Manifest.permission.READ_EXTERNAL_STORAGE))) {
+//
+//            } else {
+//                ActivityCompat.requestPermissions(getActivity(),
+//                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+//                        REQUEST_PERMISSIONS);
+//            }
+//        }else {
+//            Log.e("Else","Else");
+//            fn_imagespath();
+//        }
+//
 		
 		setHasOptionsMenu(true);
 		
@@ -112,81 +127,177 @@ public class GalleryFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
-    public ArrayList<Model_images> fn_imagespath() {
-        al_images.clear();
+//    public ArrayList<Model_images> fn_imagespath() {
+//        al_images.clear();
+//
+//        int int_position = 0;
+//        Uri uri;
+//        Cursor cursor;
+//        int column_index_data, column_index_folder_name;
+//
+//        String absolutePathOfImage = null;
+//        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//
+//        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+//
+//        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+//        cursor = getContext().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+//
+//        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+//        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+//        while (cursor.moveToNext()) {
+//            absolutePathOfImage = cursor.getString(column_index_data);
+//            Log.e("Column", absolutePathOfImage);
+//            Log.e("Folder", cursor.getString(column_index_folder_name));
+//
+//            for (int i = 0; i < al_images.size(); i++) {
+//                if (al_images.get(i).getStr_folder().equals(cursor.getString(column_index_folder_name))) {
+//                    boolean_folder = true;
+//                    int_position = i;
+//                    break;
+//                } else {
+//                    boolean_folder = false;
+//                }
+//            }
+//
+//            if (boolean_folder) {
+//
+//                ArrayList<String> al_path = new ArrayList<>();
+//                al_path.addAll(al_images.get(int_position).getAl_imagepath());
+//                al_path.add(absolutePathOfImage);
+//                al_images.get(int_position).setAl_imagepath(al_path);
+//
+//            } else {
+//                ArrayList<String> al_path = new ArrayList<>();
+//                al_path.add(absolutePathOfImage);
+//                Model_images obj_model = new Model_images();
+//                obj_model.setStr_folder(cursor.getString(column_index_folder_name));
+//                obj_model.setAl_imagepath(al_path);
+//                al_images.add(obj_model);
+//            }
+//        }
+//
+//        for (int i = 0; i < al_images.size(); i++) {
+//            Log.e("FOLDER", al_images.get(i).getStr_folder());
+//            for (int j = 0; j < al_images.get(i).getAl_imagepath().size(); j++) {
+//                Log.e("FILE", al_images.get(i).getAl_imagepath().get(j));
+//            }
+//        }
+//        obj_adapter = new Adapter_PhotosFolder(getContext(),al_images);
+//        gv_folder.setAdapter(obj_adapter);
+//        return al_images;
+//    }
 
-        int int_position = 0;
-        Uri uri;
-        Cursor cursor;
-        int column_index_data, column_index_folder_name;
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        switch (requestCode) {
+//            case REQUEST_PERMISSIONS: {
+//                for (int i = 0; i < grantResults.length; i++) {
+//                    if (grantResults.length > 0 && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+//                        fn_imagespath();
+//                    } else {
+//                        Toast.makeText(getContext(), "The app was not allowed to read or write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//            }
+//        }
+//    }
+private void setGridAdapter(String path) {
+    // Create a new grid adapter
+    gridItems = createGridItems(path);
+    MyGridAdapter adapter = new MyGridAdapter(getContext(), gridItems);
 
-        String absolutePathOfImage = null;
-        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    // Set the grid adapter
+    gridView.setAdapter(adapter);
 
-        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
-
-        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
-        cursor = getContext().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
-
-        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-        while (cursor.moveToNext()) {
-            absolutePathOfImage = cursor.getString(column_index_data);
-            Log.e("Column", absolutePathOfImage);
-            Log.e("Folder", cursor.getString(column_index_folder_name));
-
-            for (int i = 0; i < al_images.size(); i++) {
-                if (al_images.get(i).getStr_folder().equals(cursor.getString(column_index_folder_name))) {
-                    boolean_folder = true;
-                    int_position = i;
-                    break;
-                } else {
-                    boolean_folder = false;
-                }
+    // Set the onClickListener
+    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (gridItems.get(position).isDirectory()) {
+                setGridAdapter(gridItems.get(position).getPath());
             }
+            else {
+                gridItems.get(position).getImage();
+            }
+        }
+    });
 
-            if (boolean_folder) {
-
-                ArrayList<String> al_path = new ArrayList<>();
-                al_path.addAll(al_images.get(int_position).getAl_imagepath());
-                al_path.add(absolutePathOfImage);
-                al_images.get(int_position).setAl_imagepath(al_path);
-
+    gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            if (gridItems.get(position).isDirectory()) {
+                Dialog dialog = new Dialog(getContext());
+                dialog.setTitle("Test");
+                dialog.setContentView(R.layout.activity_recognition);
+                dialog.show();
+                return false;
             } else {
-                ArrayList<String> al_path = new ArrayList<>();
-                al_path.add(absolutePathOfImage);
-                Model_images obj_model = new Model_images();
-                obj_model.setStr_folder(cursor.getString(column_index_folder_name));
-                obj_model.setAl_imagepath(al_path);
-                al_images.add(obj_model);
+                Dialog dialog = new Dialog(getContext());
+                dialog.setTitle("Test");
+                dialog.setContentView(R.layout.activity_reset_password);
+                dialog.show();
+                return false;
+            }
+        }
+    });
+}
+
+    private List<GridViewItem> createGridItems(String directoryPath) {
+        List<GridViewItem> items = new ArrayList<>();
+
+        // List all the items within the folder.
+        File[] files = new File(directoryPath).listFiles(new ImageFileFilter());
+        for (File file : files) {
+
+            // Add the directories containing images or sub-directories
+            if (file.isDirectory()
+                    && file.listFiles(new ImageFileFilter()).length > 0) {
+
+                items.add(new GridViewItem(file.getAbsolutePath(), true, null));
+            }
+            // Add the images
+            else {
+                Bitmap image = BitmapHelper.decodeBitmapFromFile(file.getAbsolutePath(),
+                        50,
+                        50);
+                items.add(new GridViewItem(file.getAbsolutePath(), false, image));
             }
         }
 
-        for (int i = 0; i < al_images.size(); i++) {
-            Log.e("FOLDER", al_images.get(i).getStr_folder());
-            for (int j = 0; j < al_images.get(i).getAl_imagepath().size(); j++) {
-                Log.e("FILE", al_images.get(i).getAl_imagepath().get(j));
-            }
-        }
-        obj_adapter = new Adapter_PhotosFolder(getContext(),al_images);
-        gv_folder.setAdapter(obj_adapter);
-        return al_images;
+        return items;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        switch (requestCode) {
-            case REQUEST_PERMISSIONS: {
-                for (int i = 0; i < grantResults.length; i++) {
-                    if (grantResults.length > 0 && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                        fn_imagespath();
-                    } else {
-                        Toast.makeText(getContext(), "The app was not allowed to read or write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
-                    }
-                }
+    /**
+     * Checks the file to see if it has a compatible extension.
+     */
+    private boolean isImageFile(String filePath) {
+        if (filePath.endsWith(".jpg") || filePath.endsWith(".png"))
+        // Add other formats as desired
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This can be used to filter files.
+     */
+    private class ImageFileFilter implements FileFilter {
+
+        @Override
+        public boolean accept(File file) {
+            if (file.isDirectory()) {
+                return true;
             }
+            else if (isImageFile(file.getAbsolutePath())) {
+                return true;
+            }
+            return false;
         }
     }
+
 }
